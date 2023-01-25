6f1bbe1ef1f9f0247859a5db6cdab0a5d3ce3f1c
Arvindan <arvindan.aswathanarayanan@freshworks.com>
2023-01-25 10:31:49 +0530

    module fed react

--- host-react-module/src/App.jsx
+++ host-react-module/src/App.jsx
@@ -1,27 +1,22 @@
-import React, { Suspense } from 'react';
-import './App.css';
-import Vaider from './components/Vaider';
-const Kylo = React.lazy(() => import('remote_react_module/Kylo'));
-const BB8 = React.lazy(() => import('remote_nextjs_module/BB8'));
+import React, { Suspense } from "react";
+import "./App.css";
+import Vaider from "./components/Vaider";
+const Kylo = React.lazy(() => import("remote_react_module/Kylo"));
 function App() {
   return (
     <>
-      <div className='Text'>
+      <div className="Text">
         This is the React container App hosted at localhost:8080
       </div>
-      <div className='Host-Container'>
+      <div className="Host-Container">
         <Vaider />
         <hr />
-        <Suspense fallback={'loading...'}>
-          <BB8 />
+        <Suspense fallback={"loading..."}>
           <hr />
           <Kylo />
         </Suspense>
-        
       </div>
-      
     </>
-      
   );
 }
 
--- host-react-module/webpack.config.js
+++ host-react-module/webpack.config.js
@@ -1,46 +1,47 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ModuleFederationPlugin = require('webpack').container.ModuleFederationPlugin;
-const deps = require('./package.json').dependencies;
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin =
+  require("webpack").container.ModuleFederationPlugin;
+const deps = require("./package.json").dependencies;
 module.exports = {
-  entry: './src/index',
+  entry: "./src/index",
   entry: {
     app: {
-      import: './src/index',
-    }
+      import: "./src/index",
+    },
   },
   cache: false,
 
-  mode: 'development',
-  devtool: 'source-map',
+  mode: "development",
+  devtool: "source-map",
 
   optimization: {
     minimize: false,
   },
 
   output: {
-    publicPath: 'auto',
+    publicPath: "auto",
     // publicPath: './dist',
   },
 
   resolve: {
-    extensions: ['.jsx', '.js', '.json', '.mjs'],
+    extensions: [".jsx", ".js", ".json", ".mjs"],
   },
 
   module: {
     rules: [
       {
         test: /\.m?js$/,
-        type: 'javascript/auto',
+        type: "javascript/auto",
         resolve: {
           fullySpecified: false,
         },
       },
       {
         test: /\.jsx?$/,
-        loader: require.resolve('babel-loader'),
+        loader: require.resolve("babel-loader"),
         exclude: /node_modules/,
         options: {
-          presets: [require.resolve('@babel/preset-react')],
+          presets: [require.resolve("@babel/preset-react")],
         },
       },
       {
@@ -52,28 +53,24 @@ module.exports = {
 
   plugins: [
     new ModuleFederationPlugin({
-      name: 'host_react_module',
-      filename: 'remoteEntry.js',
+      name: "host_react_module",
+      filename: "remoteEntry.js",
       remotes: {
         // The following is when running remote at Vercel
-        remote_nextjs_module: 'remote_nextjs_module@https://remote-nextjs-module.vercel.app/_next/static/chunks/remoteEntry.js',
-        remote_react_module: 'remote_react_module@https://remote-react-module.vercel.app/RemoteEntry.js',
+        //remote_react_module: 'remote_react_module@https://remote-react-module.vercel.app/RemoteEntry.js',
         // The following is when running locally
         // remote_nextjs_module: 'remote_nextjs_module@http://localhost:8081/_next/static/chunks/remoteEntry.js',
-        // remote_react_module: 'remote_react_module@http://localhost:8082/remoteEntry.js',
+        remote_react_module:
+          "remote_react_module@http://localhost:9015/remoteEntry.js",
       },
-      exposes: {
-      },
+      exposes: {},
       shared: {
-        react: {
-          // Notice shared are NOT eager here.
-          requiredVersion: false,
-          singleton: true,
-        },
+        react: { singleton: true, requiredVersion: "18.0.0" },
+        "react-dom": { singleton: true, requiredVersion: "18.0.0" },
       },
     }),
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: "./public/index.html",
     }),
   ],
 };
